236bfa21d11cfa444fa7653a76d0bcaf3e9cbcbd
acerohernan <contacto.acero.hernan@gmail.com>
2022-11-09 13:23:40 -0500

    build: adding the cerbot service in docker compose file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common/decorators';
 
 @Controller('/')
 export class AppController {
+  @Get()
+  async root() {
+    return `<h1>Welcome to MiTienda API</h1>`;
+  }
+
   @Get('status')
   async getStatus() {
     return 'OK';
