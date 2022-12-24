825a1b86f2dfb2bd4a0adb6261ae5af2b879fbca
acerohernan <contacto.acero.hernan>
2022-12-24 09:42:53 -0500

    ci: adding scripts to codedeploy pipeline

---
+++ scripts/application_stop.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+
+#Start the application
+echo "Stoping the application..."
+echo "Application stoped!"
---
+++ scripts/before_install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+#See the directory
+pwd
+
+#List the files
+ls -1
